62b4db7356d2b6033aab0bf926cfd2a156cb9079
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2019-06-14 08:02:01 +0600

    Added script for scroll to the top - Добавлен скрипт для кнопки скролла вверх

--- scripts/client/main.js
+++ scripts/client/main.js
@@ -42,3 +42,18 @@ $(document).ready(function () {
         }
     });
 });
+
+$(window).load(function () {
+
+    var winHeight = $(document).height();
+    var step = 4;
+    var timeToScroll = winHeight / step;
+
+    $('.scrolltop').on('click', function () {
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, timeToScroll);
+    });
+
+});
